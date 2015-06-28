c2e2dfcb05e196645a6cc3b2451e1330b61f6af4
Volker Strobel <volker.strobel87@gmail.com>
2015-06-28 19:54:59 +0200

    Lost track

--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,59 @@
 import pandas as pd
 import numpy as np
+import bandits
+
+def test_policy(policy, runid, ids):
+
+    chosen_arms = [0] * len(ids)
+    rewards = [0] * len(ids)
+
+    for idx in ids:
+
+        # Print progress
+        if idx % 1000 == 0:
+            print(idx)
+        
+        # Select and save arm
+        chosen_arm = policy.select_arm()
+        chosen_arms[idx] = chosen_arm
+
+        # Get and save reward
+        print("chosen_arm:", chosen_arm)
+        # reward = policy.arms[chosen_arm].draw(runid, idx)
+
+        reward = policy.draw(runid, idx)
+        
+        rewards[idx] = reward
+
+        print(reward)
+        
+        policy.update(chosen_arm, reward)
+
+    results = pd.DataFrame({"chosen_arms": chosen_arms,
+                            "rewards": rewards})
+
+    results.to_csv("results" + str(runid) + ".csv")
+
+    return results
+
 
 runid = 188
 
-df = pd.read_csv("results_ucb" + str(runid) + ".csv")
+df = pd.read_csv("results/results_ucb" + str(runid) + ".csv")
 
 print(df.rewards.mean())
+
+
+def main():
+
+    ids = range(10000)
+
+    all_arm_properties = bandits.create_all_arm_properties()
+    all_arms = [bandits.BanditArm(prop) for prop in all_arm_properties]
+
+    linear_policy = bandits.LinearPolicy(all_arms, "contexts/context_188.csv")
+    test_policy(linear_policy, 188, ids)
+
+if __name__ == "__main__":
+
+    main()
